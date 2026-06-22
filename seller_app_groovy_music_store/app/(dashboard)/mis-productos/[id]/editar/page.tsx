b6f0df88b81f6e89020135dce6002f5c310d06fd
Juan Francisco Mitzig <juanmitzig14@gmail.com>
2026-06-22 11:23:15 -0300

    style: renombrar boton 'Desactivar producto' a 'Eliminar producto' en el vendedor

--- seller_app_groovy_music_store/app/(dashboard)/mis-productos/[id]/editar/page.tsx
+++ seller_app_groovy_music_store/app/(dashboard)/mis-productos/[id]/editar/page.tsx
@@ -45,7 +45,7 @@ export default async function EditarProductoPage({
           type="submit"
           className="text-red-500 border border-red-500 px-4 py-2 rounded hover:bg-red-50"
         >
-          Desactivar producto
+          Eliminar producto
         </button>
       </form>
     </div>
